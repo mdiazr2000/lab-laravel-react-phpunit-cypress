6968c4c18abe803b8caecc35b8b1418df2233e44
Marianela Diaz <mdiazr2000@gmail.com>
2022-05-07 20:34:04 -0400

    Add laravel web socket Add laravel echo for send notifications to frontend

---
+++ instashare/app/Events/FileZipped.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\FilesUser;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class FileZipped implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * The file instance.
+     *
+     * @var FilesUser
+     */
+    public $fileUser;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($fileUser)
+    {
+        $this->fileUser = $fileUser;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('fileszipped.'.$this->fileUser->user->email);
+    }
+}
